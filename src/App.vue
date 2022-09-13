26e87a083b5c494db0913ba6f30c0ca5a3e74105
VRCarlos21 <vazquezrosascarlosalexis@gmail.com>
2022-09-12 23:54:21 -0500

    vue3 contador 10A

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+  export default {
+   data: () => ({
+    contador :10,
+   }),
+   methods:{
+    saludar(){
+      console.log("Hola 1oA");  
+    },
+    incrementar(){
+      this.contador++;
+    },
+    decrementar(){
+      this.contador--;
+    },
+    resetear(){
+      this.contador=10;
+    }
+   }
+  };
+</script>
+
+<template>
+  <img src="./assets/logo.svg" alt="Logo VUE" width="125"
+  height="125">
+  <div class="container">
+    <small>
+    <h1>Hola desde VUE.js de Carlos Alexis Vazquez Rosas</h1>
+    </small>
+    <hr>
+    <h2>Contador {{ contador }}</h2>
+    <button v-on:click ="incrementar()">+1</button>
+    <button v-on:click ="decrementar()">-1</button>
+    <button v-on:click ="resetear()">Reset</button>
+    </div>
+</template>
+
+<style scoped>
+
+  .container{
+    text-align: center;
+  }
+
+  small{
+    color: #666;
+  }
+
+  h1{
+    font-weight: bolder;
+  }
+
+  h2{
+    font-size: 2.5rem;
+  }
+  button{
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0px 8px;
+  }
+</style>
